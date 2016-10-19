386bf175d570773101ccb82e4df2cc1123f6ae82
lammn <mnlam@lockon-vn.com>
2016-10-19 17:21:34 +0700

    commit for renew this plugin

--- Controller/Admin/RelatedProductController.php
+++ Controller/Admin/RelatedProductController.php
@@ -11,14 +11,30 @@
 
 namespace Plugin\RelatedProduct\Controller\Admin;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Eccube\Application;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception as HttpException;
 
 class RelatedProductController
 {
-    public function searchProduct(Application $app, Request $request)
+    /**
+     * @param Application $app
+     * @param Request $request
+     * @param integer     $page_no
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function searchProduct(Application $app, Request $request, $page_no = null)
     {
-        if ($request->isXmlHttpRequest()) {
+        if (!$request->isXmlHttpRequest()) {
+            return null;
+        }
+
+        $app['monolog']->addDebug('search product start.');
+        $pageCount = $app['config']['default_page_count'];
+        $session = $app['session'];
+        if ('POST' === $request->getMethod()) {
+            $page_no = 1;
             $searchData = array(
                 'id' => $request->get('id'),
             );
@@ -26,41 +42,39 @@ class RelatedProductController
                 $Category = $app['eccube.repository.category']->find($categoryId);
                 $searchData['category_id'] = $Category;
             }
-
-            /** @var $Products \Eccube\Entity\Product[] */
-            $Products = $app['eccube.repository.product']
-                ->getQueryBuilderBySearchDataForAdmin($searchData)
-                ->getQuery()
-                ->getResult();
-
-            // 表示されている商品は検索結果に含めない
-            $productId = $request->get('product_id');
-            $ProductsData = array();
-            $count = count($Products);
-            $i = 0;
-            for($i = 0; $i < $count; $i++) {
-                $Product = $Products[$i];
-                if ($Product->getId() != $productId) {
-                    $ProductsData[] = $Product;
-                }
-                if ($i >= 10) {
-                    break;
-                }
+            $session->set('eccube.plugin.related_product.product.search', $searchData);
+            $session->set('eccube.plugin.related_product.product.search.page_no', $page_no);
+        } else {
+            $searchData = (array)$session->get('eccube.plugin.related_product.product.search');
+            if (is_null($page_no)) {
+                $page_no = intval($session->get('eccube.plugin.related_product.product.search.page_no'));
+            } else {
+                $session->set('eccube.plugin.related_product.product.search.page_no', $page_no);
             }
-
-            $message = '';
-            if ($count > $i) {
-                $message = '検索結果の上限を超えています。検索条件を設定してください。';
-            }
-
-            return $app->renderView(
-                'RelatedProduct/Resource/template/Admin/modal_result.twig',
-                array(
-                    'Products' => $ProductsData,
-                    'message' => $message,
-                )
-            );
-
         }
+
+        $qb = $app['eccube.repository.product']->getQueryBuilderBySearchDataForAdmin($searchData);
+
+        /** @var \Knp\Component\Pager\Pagination\SlidingPagination $pagination */
+        $pagination = $app['paginator']()->paginate(
+            $qb,
+            $page_no,
+            $pageCount,
+            array('wrap-queries' => true)
+        );
+        /** @var ArrayCollection */
+        $arrProduct = $pagination->getItems();
+
+        if (count($arrProduct) == 0) {
+            $app['monolog']->addDebug('search product not found.');
+        }
+
+        $paths = array();
+        $paths[] = $app['config']['template_admin_realdir'];
+        $app['twig.loader']->addLoader(new \Twig_Loader_Filesystem($paths));
+
+        return $app->render('RelatedProduct/Resource/template/admin/modal_result.twig', array(
+            'pagination' => $pagination
+        ));
     }
 }
--- PluginManager.php
+++ PluginManager.php
@@ -12,29 +12,109 @@
 namespace Plugin\RelatedProduct;
 
 use Eccube\Plugin\AbstractPluginManager;
+use Symfony\Component\Filesystem\Filesystem;
 
 class PluginManager extends AbstractPluginManager
 {
 
+    /**
+     * @var string コピー元リソースディレクトリ
+     */
+    private $origin;
+
+    /**
+     * @var string コピー先リソースディレクトリ
+     */
+    private $target;
+
+    /**
+     * PluginManager constructor.
+     */
+    public function __construct()
+    {
+        // コピー元のディレクトリ
+        $this->origin = __DIR__.'/Resource/assets';
+        // コピー先のディレクトリ
+        $this->target = __DIR__ . '/../../../html/plugin/relatedproduct';
+    }
+
+    /**
+     * プラグインインストール時の処理
+     *
+     * @param $config
+     * @param $app
+     * @throws \Exception
+     */
     public function install($config, $app)
     {
-        $this->migrationSchema($app, __DIR__ . '/Migration', $config['code']);
+        // リソースファイルのコピー
+        $this->copyAssets($app);
     }
 
+    /**
+     * プラグイン削除時の処理
+     *
+     * @param $config
+     * @param $app
+     */
     public function uninstall($config, $app)
     {
-        $this->migrationSchema($app, __DIR__ . '/Migration', $config['code'], 0);
+        $this->migrationSchema($app, __DIR__.'/Resource/doctrine/migration', $config['code'], 0);
+        // リソースファイルの削除
+        $this->removeAssets($app);
     }
 
+    /**
+     * プラグイン有効時の処理
+     *
+     * @param $config
+     * @param $app
+     * @throws \Exception
+     */
     public function enable($config, $app)
     {
+        $this->migrationSchema($app, __DIR__.'/Resource/doctrine/migration', $config['code']);
     }
 
+    /**
+     * プラグイン無効時の処理
+     *
+     * @param $config
+     * @param $app
+     */
     public function disable($config, $app)
     {
     }
 
+    /**
+     * プラグイン更新時の処理
+     * @param $config
+     * @param $app
+     */
     public function update($config, $app)
     {
+        $this->migrationSchema($app, __DIR__.'/Resource/doctrine/migration', $config['code'], 0);
+    }
+
+    /**
+     * リソースファイル等をコピー
+     *
+     * @param $app
+     */
+    private function copyAssets($app)
+    {
+        $file = new Filesystem();
+        $file->mirror($this->origin, $this->target.'/assets');
+    }
+
+    /**
+     * コピーしたリソースファイルなどを削除
+     *
+     * @param $app
+     */
+    private function removeAssets($app)
+    {
+        $file = new Filesystem();
+        $file->remove($this->target);
     }
 }
