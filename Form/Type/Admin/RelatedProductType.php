7294a92021015905037bd36e13c355c1459a6fcc
Chihiro Adachi <8196725+chihiro-adachi@users.noreply.github.com>
2018-09-26 19:10:40 +0900

    cs fix

--- Form/Type/Admin/RelatedProductType.php
+++ Form/Type/Admin/RelatedProductType.php
@@ -23,7 +23,6 @@ use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Translation\TranslatorInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
